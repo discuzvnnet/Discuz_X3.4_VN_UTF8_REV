4c3cf4b1b762a72a140615bde11163f56f41cb65
discuzvn <discuzvnnet@gmail.com>
2022-01-15 14:35:17 +0700

    R20211231

--- upload/source/discuz_version.php
+++ upload/source/discuz_version.php
@@ -13,9 +13,8 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.4');
-	define('DISCUZ_RELEASE', '20210926');
+	define('DISCUZ_RELEASE', '20211231');
 	define('DISCUZ_FIXBUG', '34000000');
-	define('DISCUZ_TH_REVISION', '69'); /*jaideejung007*/
 }
 
 ?>
